9233ebd965101ef4630fd01112d16363e12c1bb3
Phil Kunz <github@philkunz.com>
2018-12-28 00:07:48 +0100

    fix(core): update

--- test/test.ts
+++ test/test.ts
@@ -2,7 +2,7 @@ import { expect, tap } from '@pushrocks/tapbundle';
 import * as weblogo from '../ts/index'
 
 tap.test('first test', async () => {
-  console.log(weblogo.standardExport)
+  
 })
 
-tap.start()
+tap.start();
