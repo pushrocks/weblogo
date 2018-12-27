8f704c43d07a38f21c3d941061813c8252f41593
Phil Kunz <github@philkunz.com>
2018-12-27 23:52:56 +0100

    fix(core): update

---
+++ ts/index.ts
@@ -0,0 +1,20 @@
+import * as plugins from './weblogo.plugins';
+
+export interface IWebLogo {
+  svgBanner: string;
+  svgSquared: string;
+  favIcon: string;
+
+}
+
+/**
+ * this class will expose the right urls for any needed logo variant within web projects
+ */
+export class WebLogo implements IWebLogo {
+  svgBanner: string;
+  svgSquared: string;
+  favIcon: string;
+  constructor(baseUrl: string) {
+
+  }
+}
